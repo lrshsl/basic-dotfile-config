e4e60630d437bac4f170a5d09f3c0bf11cec92ef
lrshsl <hoeslilars@gmail.com>
2023-05-13 21:51:57 +0200

    add spell & more changes

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -31,6 +31,9 @@ require('packer').startup(function(use)
 
     use 'tpope/vim-commentary'
 
+    use 'Exafunction/codeium.vim'
+
+     use 'sirver/UltiSnips'
 end)
 
 
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -8,5 +8,13 @@ set ts=4 sts=4 sw=4 et
 set relativenumber
 set number
 
+set nowrap
+
+"set foldenable
+"set foldmethod=indent
+
+set statusline+=\{…\}%3{codeium#GetStatusString()}
+
+set listchars=tab:▸\ ,eol:¬
 
 ]]
